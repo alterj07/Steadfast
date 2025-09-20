184699800657a3643e2bef0c2bb0f7d2498727c1
Jayden Chun <jayden.h.chun@gmail.com>
2025-09-19 22:31:40 -0500

    tried implementing color scheme(not fixed yet)

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,16 +1,27 @@
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { ScrollView, StyleSheet, Switch, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-
 export default function SettingScreen() {
-  return (
-    <SafeAreaProvider style = {{backgroundColor: 'pink'}}>
-      <SafeAreaView style = {styles.container} edges = {['top']}>
-        <ScrollView style = {styles.scrollView}>
-          <Text style = {styles.welcomeText}>Settings</Text>
-        </ScrollView>
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
+    const { colorScheme, setColorScheme, toggleColorScheme } = useColorScheme();
+  
+    // Convert string to boolean for Switch component
+    const isDarkMode = colorScheme === 'dark';
+
+    return (
+        <SafeAreaProvider style = {{backgroundColor: 'pink'}}>
+            <SafeAreaView style = {styles.container} edges = {['top']}>
+                <ScrollView style = {styles.scrollView}>
+                <Text style = {styles.welcomeText}>Settings</Text>
+                <View style = {styles.settingsView}>
+                    <View style = {styles.settingsComponent}>
+                        <Text style = {styles.settingsText}>Dark</Text>
+                        <Switch value={isDarkMode} onValueChange={toggleColorScheme} trackColor={{false: '#000000', true: '#FFFFFF'}} ios_backgroundColor="#000000" thumbColor = {colorScheme === 'dark' ? 'black' : 'white'} style = {styles.switch}/>
+                    </View>
+                </View>
+                </ScrollView>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -24,5 +35,21 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 30,
     textAlign: 'center'
+  },
+  settingsView: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 20
+  },
+  switch: {
+    alignSelf: 'center',
+  },
+  settingsComponent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  settingsText: {
+    fontWeight: 'bold',
+    padding: 5
   }
 });
